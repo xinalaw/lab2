ea869f4e43f3f0c18cbdc32fddbe165760e4cd1f
xinal <xinal@LAPTOP-UK663889>
2023-03-01 20:01:15 +0300

    Добавьте файлы проекта.

---
+++ firstoflaba2/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    internal class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set { if (value == 'm' | value == 'f') gender = value; }
+        }
+        public Person() { }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + DateOfBirth + " " + gender;
+        }
+        public void Read()
+        {
+            Console.WriteLine("Enter a name: ");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("Enter a surname");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Enter date of birth in (yyyy-mm-dd) format: ");
+            DateOfBirth = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter sex(m/f): ");
+            gender = char.Parse(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {FirstName} Фамилия: {LastName} Возраст: {Age(DateOfBirth)} Пол: {gender}");
+        }
+        public int Age(DateTime BDay)
+        {
+            int age = DateTime.Now.Year - BDay.Year;
+            if (DateTime.Now.Day < BDay.Day)
+            {
+                if (DateTime.Now.Month < BDay.Month)
+                {
+                    age--;
+                }
+            }
+            return age;
+        }
+    }
+}
---
+++ firstoflaba2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Lab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.Write("Enter a number of people: ");
+            int m = int.Parse(Console.ReadLine());
+            Person[] people = new Person[m];
+            for (int i = 0; i < m; i++)
+            {
+                people[i] = new Person();
+                people[i].Read();
+            }
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine(people[i].FirstName);
+                Console.WriteLine(people[i].LastName);
+                Console.WriteLine(people[i].Age(people[i].DateOfBirth));
+                Console.WriteLine(people[i].Gender);
+            }
+
+        }
+    }
+}
